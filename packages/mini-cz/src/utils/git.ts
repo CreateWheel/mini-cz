7b3c9e2ee58a0517f4c58ddb1f762f6ddccc6970
so1ve <nn_201312@163.com>
2023-01-19 20:20:05 +0800

    🐛 fix(core): unadded changes

--- packages/mini-cz/src/utils/git.ts
+++ packages/mini-cz/src/utils/git.ts
@@ -2,5 +2,7 @@ import { exec } from "clerc/toolkit";
 
 export const haveUnaddedChanges = async () => {
   const { stdout } = await exec("git", ["status", "--porcelain"]);
-  return stdout.length > 0;
+  const unaddedChanges = stdout.split("\n")
+    .some(s => s[1] === "M");
+  return unaddedChanges;
 };
