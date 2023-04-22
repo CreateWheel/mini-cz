7901a9b3e7148fcc0245aa83e49a037a6bdbd2b0
so1ve <i@mk1.io>
2023-04-22 16:03:59 +0800

    feat: set cli name

--- packages/mini-cz/src/cli.ts
+++ packages/mini-cz/src/cli.ts
@@ -5,6 +5,7 @@ import { description, version } from "../package.json";
 import { commitCommand } from "./commands";
 
 const _cli = Clerc.create("mcz", description, version)
+  .name("Mini CZ")
   .use(helpPlugin())
   .use(notFoundPlugin())
   .use(strictFlagsPlugin())
