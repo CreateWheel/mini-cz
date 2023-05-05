c47191cc9e17fc8358a24befdf8a79c258cd23c6
so1ve <i@mk1.io>
2023-05-05 13:49:13 +0800

    🚀 perf(mini-cz): use yajuu instead c12 for performance

--- packages/mini-cz/src/utils/config.ts
+++ packages/mini-cz/src/utils/config.ts
@@ -1,4 +1,4 @@
-import { loadConfig } from "c12";
+import { loadConfig } from "yajuu";
 
 import type { Config } from "../types";
 
@@ -8,8 +8,10 @@ export const resolveConfig = async () => {
   const { config } = await loadConfig<Config>({
     name: "mini-cz",
   });
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
   if (Object.keys(config!).length === 0) {
     errorAndExit("Config file not found!");
   }
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
   return config!;
 };
